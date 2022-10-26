80406b797fea2ca794c9c6c5bfbd11412d5a62c7
AndradeGarcia1 <116766299+AndradeGarcia1@users.noreply.github.com>
2022-10-26 15:48:36 -0600

    f

---
+++ js/Script.js
@@ -0,0 +1,33 @@
+//boton
+var bbtt = document.getElementById("boton");
+bbtt.addEventListener("click",calcular);
+
+
+
+function calcular(){
+    //variables globales
+var a = parseFloat(document.getElementById("a").value);
+var b = parseFloat(document.getElementById("b").value);
+var c = parseFloat(document.getElementById("c").value);
+    //variables privadas
+    let comparador = ((b*b) - (4*a*c));
+    let real = false;
+    let imaginario = false;
+    
+    if(comparador <= 0){
+       imaginario = true;
+        
+       }else{
+       real = true;
+       }
+    
+    if(imaginario == true){
+       comparador = comparador*(-1);
+        alert("Lasolucion imaginaria es: "+ (-b/(2*a))+" + "+((comparador)/(2*a))+"i y "+(-b/(2*a))+" - "+(comparador*comparador/(2*a)));
+       }
+    if(real == true){
+        
+            alert("Solucion real es: x1 = "+ (-b + (comparador)) / (2*a) +"x2 = "+(-b - (comparador)) / (2*a));
+    }   
+    
+}
